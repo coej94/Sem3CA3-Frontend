20736970aafe2b0c0b33742198a7b2574e40af99
Staalet <staalthomas1@gmail.com>
2017-04-07 13:34:44 +0200

    Products and Details done after a great depression and reflection on goals in life

--- src/pages/Details.js
+++ src/pages/Details.js
@@ -4,30 +4,39 @@
 
 import React from "react";
 import bookstore from "../stores/Bookstore";
+import {Link} from "react-router";
 
 class Details extends React.Component {
-
-    constructor() {
-        super();
-        this.state = {
-            book: {
-                title: "",
-                info: "",
-                moreInfo: "",
-            }
-        };
-    }
+    //
+    // constructor() {
+    //     super();
+    //     this.state = {
+    //         book: {
+    //             title: "",
+    //             info: "",
+    //             moreInfo: "",
+    //         }
+    //     };
+    // }
 
     render() {
-        let id = this.props.book.params.id;
-        let book = bookstore.books.filter((book, index) => {
-            return index === Number(id);
+        let id = this.props.params.id;
+        let book = bookstore.books.filter((book) => {
+            if (Number(book.id) === Number(id)) {
+                return book;
+            }
         })[0];
+
+        if (book == null) {
+            book = {title: "", info: "", moreInfo: ""}
+        }
         return (
             <div>
                 <h3 style={{color: "steelblue"}}>Detailed info for the title: {book.title}</h3>
                 <h4> {book.info}</h4>
                 <h4>{book.moreInfo}</h4>
+                <br/>
+                <Link to="/products">Back to Products</Link>
             </div>
         );
     }
